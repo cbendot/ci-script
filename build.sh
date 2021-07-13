9e6c888f3295a0d07209c32ea7ae80e0c03283cf
ben863 <r.budhiono@gmail.com>
2021-07-13 10:33:22 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,30 +2,45 @@
 #
 # Copyright (C) 2021 a xyzprjkt property
 #
-echo "Downloading few Dependecies . . ."
-git clone --depth=1 https://github.com/cbendot/elastics-toolchain llvm
-git clone --depth=1 https://github.com/cbendot/kernel_asus_sdm660 hard
 
-# Main
-KERNEL_ROOTDIR=$(pwd)/hard # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG=ElasticsPerf_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=$(pwd)/llvm # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER=ben863 # Change with your own name or else.
-export KBUILD_BUILD_HOST=LiteSpeed-CloudLinux # Change with your own hostname.
+# Needed Secret Variable
+# KERNEL_NAME | Your kernel name
+# KERNEL_SOURCE | Your kernel link source
+# KERNEL_BRANCH  | Your needed kernel branch if needed with -b. eg -b eleven_eas
+# DEVICE_CODENAME | Your device codename
+# DEVICE_DEFCONFIG | Your device defconfig eg. lavender_defconfig
+# ANYKERNEL | Your Anykernel link repository
+# TG_TOKEN | Your telegram bot token
+# TG_CHAT_ID | Your telegram private ci chat id
+# BUILD_USER | Your username
+# BUILD_HOST | Your hostname
+
+echo "Downloading few Dependecies . . ."
+# Kernel Sources
+git clone --depth=1 $KERNEL_SOURCE $KERNEL_BRANCH $DEVICE_CODENAME
+git clone --depth=1 https://github.com/cbendot/elastics-toolchain Elastics # Elastics set as Clang Default
+
+# Main Declaration
+KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=$DEVICE_DEFCONFIG # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=$(pwd)/Elastics # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=$BUILD_USER # Change with your own name or else.
+export KBUILD_BUILD_HOST=$BUILD_HOST # Change with your own hostname.
 
 # Main Declaration
 CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
 export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
-IMAGE=$(pwd)/hard/out/arch/arm64/boot/Image.gz-dtb
+IMAGE=$(pwd)/$DEVICE_CODENAME/out/arch/arm64/boot/Image.gz-dtb
 DATE=$(date "+%B %-d, %Y")
+ZIP_DATE=$(date +"%Y%m%d-%H%M")
 START=$(date +"%s")
 
 # Checking environtment
 # Warning !! Dont Change anything there without known reason.
 function check() {
 echo ================================================
-echo Kernel Compiler Started!
+echo xKernelCompiler
 echo version : rev1.5 - gaspoll modified
 echo ================================================
 echo BUILDER NAME = ${KBUILD_BUILD_USER}
@@ -37,66 +52,67 @@ echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
 echo ================================================
 }
 
-# Compiler
-function compile() {
+# Telegram
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 
-   curl -s -X POST "https://api.telegram.org/bot${token}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAEChbdg3-SJAabmOMYa5Pax18UWLnLBVAACpgIAApk4AAFXSahPNJ_y_k0gBA" \
-        -d chat_id="${chat_id}"
+tg_post_msg() {
+  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+  -d "disable_web_page_preview=true" \
+  -d "parse_mode=html" \
+  -d text="$1"
+}
 
-# Private CI
-   curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
-        -d chat_id="${chat_id}" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>🔨 Building Kernel Started!</b>%0ABuilder Name: <code>${KBUILD_BUILD_USER}</code>%0ABuilder Host: <code>${KBUILD_BUILD_HOST}</code>%0ABuild Date: <code>$DATE</code>%0ABuild started on: <code>Drone CI</code>%0AClang Rootdir : <code>${CLANG_ROOTDIR}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>%0ACompiler Info:%0A<code>${KBUILD_COMPILER_STRING}</code>%0A%0A1:00 ●━━━━━━─────── 2:00 ⇆ㅤㅤㅤ ㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤㅤㅤ↻"
+# Post Main Information
+tg_post_msg "<b>🔨 Building Kernel Started!</b>%0A<b>Builder Name: </b><code>${KBUILD_BUILD_USER}</code>%0A<b>Builder Host: </b><code>${KBUILD_BUILD_HOST}</code>%0A<b>Build For: </b><code>$DEVICE_CODENAME</code>%0A<b>Build Date: </b><code>$DATE</code>%0A<b>Build started on: </b><code>Drone CI</code>%0A<b>Clang Rootdir : </b><code>${CLANG_ROOTDIR}</code>%0A<b>Kernel Rootdir : </b><code>${KERNEL_ROOTDIR}</code>%0A<b>Compiler Info:</b>%0A<code>${KBUILD_COMPILER_STRING}</code>%0A%0A1:00 ●━━━━━━─────── 2:00 ⇆ㅤㅤㅤ ㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤㅤㅤ↻"
 
-  cd ${KERNEL_ROOTDIR}
-  make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-  make -j$(nproc) ARCH=arm64 O=out \
-  	CC=${CLANG_ROOTDIR}/bin/clang \
-	AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-	STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+# Compile
+compile(){
+tg_post_msg "<b>xKernelCompiler:</b><code>Compilation has started"
+cd ${KERNEL_ROOTDIR}
+make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+make -j$(nproc) ARCH=arm64 O=out \
+    CC=${CLANG_ROOTDIR}/bin/clang \
+    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
+  	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+  	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
+  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
+    STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
+    CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
 	exit 1
    fi
-        git clone --depth=1 https://github.com/cbendot/AnyKernel3 AnyKernel
-	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+
+  git clone --depth=1 $ANYKERNEL AnyKernel
+	cp $IMAGE AnyKernel
 }
 
 # Push kernel to channel
 function push() {
     cd AnyKernel
     ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot${token}/sendDocument" \
-        -F chat_id="${chat_id}" \
+    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+        -F chat_id="$TG_CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="✅ <code>$(($DIFF / 60)) minute(s) $(($DIFF % 60)) second(s)</code>  <code>$DATE</code>"
+        -F caption="✅ $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)"
 }
-        
 # Fin Error
 function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
-        -d chat_id="${chat_id}" \
+    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
-        -d text="❌ Build throw an error(s)%0A%0A<code>$(($DIFF / 60)) minute(s) $(($DIFF % 60)) second(s) </code>"
-
+        -d text="❌ Build throw an error(s)"
     exit 1
 }
 
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 ElasticsPerf-HMP-${DATE}.zip *
+    zip -r9 $KERNEL_NAME-${ZIP_DATE}.zip *
     cd ..
 }
 check
