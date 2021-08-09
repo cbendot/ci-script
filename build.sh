06c65e1c5903c789ef420e3af1a1ef099a5ec881
ben863 <r.budhiono@gmail.com>
2021-08-09 10:40:44 +0700

    Update build.sh
    revert new script

--- build.sh
+++ build.sh
@@ -72,15 +72,12 @@ cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
-    HOSTCC=${CLANG_ROOTDIR}/bin/clang \
-    HOSTCXX=${CLANG_ROOTDIR}/bin/clang++ \
     AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-        AS=${CLANG_ROOTDIR}/bin/llvm-as \
+    AS=${CLANG_ROOTDIR}/bin/llvm-as \
   	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
         OBJSIZE=${CLANG_ROOTDIR}/bin/llvm-size \
   	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-    READELF=${CLANG_ROOTDIR}/bin/llvm-readelf \
+  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \   
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
