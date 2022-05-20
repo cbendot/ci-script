f8405c8dcf264aceedbed2a13751e0e44c0815c5
ben863 <r.budhiono@gmail.com>
2022-05-20 08:33:05 +0700

    use ARM32 because COMPAT not support which old kernel
    Change-Id: Icadb07deca1f2cf932ab910b042f5f3c3c74d9bd
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ make -j$(nproc) ARCH=arm64 O=out \
   	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-    CROSS_COMPILE_COMPAT=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
