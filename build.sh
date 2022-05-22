896fc6f281e6dfb0e126bc9fa66b46bf2510cc20
ben863 <r.budhiono@gmail.com>
2022-05-22 18:26:23 +0700

    nuke LTO and PGO
    Change-Id: I8b56d0288bcfae34c5330cb0040c165cf1d7a0d6
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- build.sh
+++ build.sh
@@ -54,8 +54,6 @@ tg_post_msg "<code>Building LLVM...</code>"
 	--defines LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
 	--projects "clang;lld;polly;compiler-rt;bolt" \
 	--targets "ARM;AArch64" \
-  --lto thin \
-  --pgo kernel-defconfig \
 	--shallow-clone \
 	--incremental \
 	--build-type "Release" \ 2>&1 | tee build.log
