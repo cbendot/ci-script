ae6b8b38c0b61b22f892ef3e85a508cc56f11a3a
ben863 <r.budhiono@gmail.com>
2022-05-27 23:14:42 +0700

    circleci: update script command && build.sh
    Change-Id: Ic0079d45d9e6a70be368da992d179bc62225bbfc
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-
-# Secret Variable for CI
-# LLVM_NAME | Your desired Toolchain Name
-# TG_TOKEN | Your Telegram Bot Token
-# TG_CHAT_ID | Your Telegram Channel / Group Chat ID
-# GH_USERNAME | Your Github Username
-# GH_EMAIL | Your Github Email
-# GH_TOKEN | Your Github Token ( repo & repo_hook )
-# GL_TOKEN | Your GitLab Token 
-# GH_PUSH_REPO_URL | Your GitHub Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/xyz-prjkt/xRageTC.git
-# GL_PUSH_REPO_URL | Your GitLab Repository
-
-# Function to show an informational message
-
 # Use tcbuild build script as LLVM Build Script.
-# https://raw.githubusercontent.com/cbendot/tcbuild/llvm-tc/build-tc.sh
+git clone https://github.com/cbendot/tcbuild -b llvm-tc && cd tcbuild
+bash build-tc.sh
