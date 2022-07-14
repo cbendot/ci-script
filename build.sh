4a654f3bc41831695743158267eaf76bb865ac0f
ben863 <r.budhiono@gmail.com>
2022-07-14 08:57:32 +0700

    switch to oc hmp
    Change-Id: I3ac44b0fb13bbb43889cb7094d416fffd894cd3b

--- build.sh
+++ build.sh
@@ -124,7 +124,7 @@ error() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 $KERNEL_NAME-HMP-${ZIP_DATE}.zip *
+    zip -r9 [OC]$KERNEL_NAME-HMP-${ZIP_DATE}.zip *
     cd ..
 
 }
