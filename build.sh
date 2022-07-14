5bb8a96527703486933b975254bdb9133896a1b5
ben863 <r.budhiono@gmail.com>
2022-07-14 07:26:28 +0700

    fix dir name clang-aosp
    Change-Id: I5381366ff78221f1c02bc745bcf427a6c6838f4d

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ echo "|| Downloading few Dependecies . . .||"
 # Kernel Sources
 git clone --depth=1 $KERNEL_SOURCE -b hmp $DEVICE_CODENAME
 # git clone --depth=1 $KERNEL_SOURCE -b eas $DEVICE_CODENAME
-git clone --depth=1 https://gitlab.com/ben863/elastics-clang clang-llvm # Elastics set as Clang Default
+git clone --depth=1 https://gitlab.com/ben863/elastics-clang clang-aosp # Elastics set as Clang Default
 # git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r450784.git clang-aosp
 # git clone --depth=1 https://gitlab.com/ben863/aosp-clang.git clang-aosp
 git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
